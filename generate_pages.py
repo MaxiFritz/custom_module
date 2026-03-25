24538cfd63110b60f6a7e861af8e1e92b3da1958
MaxiFritz <98812255+MaxiFritz@users.noreply.github.com>
2026-03-25 11:01:53 +0100

    Update generate_pages.py

--- generate_pages.py
+++ generate_pages.py
@@ -71,7 +71,7 @@ EVENTS = [
     ("World Cup", "2026-06-11"),
     ("Rugby League WC", "2026-10-15"),
     ("YOG Dakar", "2026-10-31"),
-    ("YOG Dakar", "2026-04-18"),
+    ("NAB", "2026-04-18"),
     ("Summer Olympics", "2028-07-14")
 ]
 
